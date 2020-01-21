67c4812291a0de02c68a9530b41230f8af8e087d
Ciakarellas <subskrypcje.maciek@gmail.com>
2020-01-22 00:02:29 +0100

    carousel using PageView. Test

--- lib/Widget/carousel.dart
+++ lib/Widget/carousel.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
-class Carousel extends StatelessWidget{
+
+class Carousel extends StatefulWidget{
+  @override
+  _CarouselState createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+
+  final PageController  ctrl = PageController(viewportFraction: 0.8);
+
+  // bym zapamietal co on robi 
+
+  String activeTag = 'active';
+  List<String> data = ['Plastik', 'Szkoło', '3', '4'];
+  int courentPage = 0;
+  @override
+  initState(){
+    ctrl.addListener((){
+      int next = ctrl.page.round();
+      if (courentPage != next){
+        setState(() {
+          courentPage = next;
+        });
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
-  return CarouselSlider(
-      height: 500.0,
-      viewportFraction: .8,
-      enableInfiniteScroll: false,
-      aspectRatio: 16/9,
-      enlargeCenterPage: true,
-      items: [1,2,3,4,5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 18.0),
-              decoration: BoxDecoration(
-                color: Colors.amber
-              ),
-              child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-            );
-          },
-        );
-      }).toList(),
+  return PageView.builder(
+      controller: ctrl,
+      itemCount: data.length,
+      itemBuilder: (context, courentIndex){
+        bool active = courentIndex == courentPage;
+        return _pageCarousel(data[courentIndex], active);
+      },
+    );
+  }
+
+  _pageCarousel(data, bool active){
+    //Animation proper
+    final double blur = active ? 30 : 0;
+    final double offset = active ? 20 : 0;
+    final double top = active ? 150 : 200;
+    final double bottom = active ? 150 : 200;
+
+    return AnimatedContainer(
+
+      duration: Duration(milliseconds: 200),
+      curve: Curves.easeInSine,
+      margin: EdgeInsets.only(top: top, bottom: bottom, right: 20,left: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: [BoxShadow(color: Colors.black26, blurRadius: blur, offset: Offset(offset, offset))]
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.redAccent
+        ),
+      ),
     );
   }
 }
