e208ee2a21e5d01aaec0cbe62212db661b29b6db
Ciakarellas <subskrypcje.maciek@gmail.com>
2020-01-27 00:34:08 +0100

    show and hide button on carousel page

--- lib/Widget/carousel_page.dart
+++ lib/Widget/carousel_page.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../Provider/recycling_provider.dart';
 
 class PageCarousel extends StatelessWidget{
 
   final bool active;
   final dataItem;
+  
+  
+
 
   PageCarousel(this.active, this.dataItem);
 
   @override
   Widget build(BuildContext context) {
 
-    final double blur = active ? 30 : 0;
-    final double offset = active ? 20 : 0;
+    final plug = Padding(padding: EdgeInsets.all(0));
     final double top = active ? 150 : 200;
     final double bottom = active ? 150 : 200;
+    final double blur = active ? 30 : 0;
+    final double offset = active ? 20 : 0;
+    final double topPading = active ? 40 : 0;
+    final button = Padding(
+              padding: EdgeInsets.fromLTRB(0, topPading, 0, 5),
+              child: MaterialButton(
+                onPressed: (){},
+                color: Colors.greenAccent,
+                child: Text('Lista'),
+                shape: StadiumBorder(),
+              ),
+            );
+    final Widget opacity = active ? button : plug;
+    
+    
     // TODO: implement build
+
+    
     return AnimatedContainer(
-      duration: Duration(milliseconds: 200),
+      duration: Duration(milliseconds: 1000),
       curve: Curves.easeInSine,
       margin: EdgeInsets.only(top: top, bottom: bottom, right: 20,left: 20),
       decoration: BoxDecoration(
@@ -34,34 +50,33 @@ class PageCarousel extends StatelessWidget{
         child: Column(
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.all(30),
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
               child: Text('Plastik'),),
             Padding(
               child: Stack(
                 alignment: Alignment.bottomCenter,
                 children: <Widget>[
-                  Container(
-                      width: 80,
-                      height: 15,
-                      decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black45,
-                          blurRadius: blur,
-                          ),
-                      ],
+                  Positioned(
+                    top: 7,
+                    left: .6,
+                    child: Opacity(
+                      opacity: .3,
+                        child: Image(
+                          color: Colors.black38,
+                          image: AssetImage('./assets/plastic-bottle.png',),
+                          width: 150.0,
+                        ),
                     ),
                   ),
-                  
                   Image( 
-                    image: AssetImage('./assets/plastic-bottle.png',),
-                    width: 150.0,
-                  )
+                      image: AssetImage('./assets/plastic-bottle.png',),
+                      width: 150.0,
+                    ),
                 ] 
               ),
               padding: EdgeInsets.all(8),
             ),
-            Padding(padding: EdgeInsets.all(1),),
+            opacity
           ],
         ),
       ),
