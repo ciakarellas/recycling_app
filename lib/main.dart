853366fa7393f5ae43d5c1969cf934d7996a774b
Ciakarellas <subskrypcje.maciek@gmail.com>
2020-02-01 08:39:21 +0100

    delete bottom navigation and add in that plase new widget BottomNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:recycling_app/Provider/recycling_provider.dart';
+import 'package:recycling_app/Widget/bottomNavBar.dart';
 
 import './View/home_page.dart';
 
@@ -12,8 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext build){
 
-    
-
     SystemChrome.setPreferredOrientations([
         DeviceOrientation.portraitUp,
       ]);
@@ -21,39 +20,7 @@ class MyApp extends StatelessWidget {
       create: (_) => new RecyclingProvider(),
       child: MaterialApp(
         home: Scaffold(
-          bottomNavigationBar: Container(
-            width: 90,
-            margin: EdgeInsets.fromLTRB(2, 0, 2, 5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(90),
-              boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 2, offset: Offset(0, 5))]
-            ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    FlatButton(
-                      onPressed: (){},
-                      child: Icon(Icons.ac_unit),
-                    ),
-                    FlatButton(
-                      onPressed: (){},
-                      child: Icon(Icons.restore),
-                    ),
-                    FlatButton(
-                      onPressed: (){},
-                      child: Icon(Icons.business),
-                    ),
-                    FlatButton(
-                      onPressed: (){},
-                      child: Icon(Icons.accessibility_new),
-                    )
-                ],
-                ),
-              ),
-          ),
+          bottomNavigationBar: BottomNavBar(),
           body: HomePage(),
         ),
       ),
