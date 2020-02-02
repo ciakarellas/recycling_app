3c33bf9adcbe6a13ca661660582c01179cb3541a
Ciakarellas <subskrypcje.maciek@gmail.com>
2020-02-02 07:45:01 +0100

    test

--- lib/View/home_page.dart
+++ lib/View/home_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recycling_app/Provider/recycling_provider.dart';
 import 'package:recycling_app/Widget/carousel.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+   final prov = Provider.of<RecyclingProvider>(context);
+   // TODO: implement if else statment to display child in this Container. 
+   // my idea is that to display correct view depend on wich screen user select
     // TODO: implement build
     return Container(
       decoration: BoxDecoration(
@@ -21,3 +26,4 @@ class HomePage extends StatelessWidget {
   }
   
 }
+
