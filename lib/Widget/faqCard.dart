5fc0dc7540161b9154d5e2bdcf0ec58eb9815b4b
Ciakarellas <subskrypcje.maciek@gmail.com>
2020-02-14 08:02:05 +0100

    anything

--- lib/Widget/faqCard.dart
+++ lib/Widget/faqCard.dart
@@ -12,24 +12,45 @@ class _FaqCardState extends State<FaqCard> {
   bool active = false;
   double height = 100;
 
+
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: AnimatedContainer(
-          duration: Duration(milliseconds: 400),
-          curve: Curves.easeInSine,
-          height: height,
-          color: Colors.white,
-          width: 200,
-          child:  Column(
-              children: <Widget>[
-      Text('testujemy co sie da'),
-      RaisedButton(onPressed: (){
-        setState(() {
-            active = !active;
-            height = active ? 100 : 200;
-        });
-      })
+    
+        return  Padding(
+          padding: EdgeInsets.all(8),
+          child: AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeInSine,
+              height: height,
+              color: Colors.white,
+              width: 200,
+              child:  Column(
+                  children: <Widget>[
+          Text('testujemy co sie da'),
+          RaisedButton(onPressed: (){
+            setState(() {
+                active = !active;
+                height = active ? 200 : 100;
+            });
+          }),
+          AnimatedContainer(
+            duration: Duration(milliseconds: 400),
+            curve: Curves.easeInSine,
+            child: Visibility(
+              maintainAnimation: true,
+              maintainState: true,
+              visible: active,
+                          child: Padding(
+                padding: EdgeInsets.fromLTRB(8, 20, 8, 8),
+                child: Column(
+                  children: <Widget>[
+                    Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+                  ],
+                ),
+          ),
+            ),
+      ),
+      
               ],
             ),
           ),
