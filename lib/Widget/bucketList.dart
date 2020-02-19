2b7b76e2d2f5c8b7efa2cf7fedf77880df52c313
Wroblewski <mwroblewski2@dxc.com>
2020-02-19 17:14:31 +0100

    change UI for serching trash list

--- lib/Widget/bucketList.dart
+++ lib/Widget/bucketList.dart
@@ -12,9 +12,9 @@ class BucketList extends StatelessWidget {
         if(trashProvider.searchingList == null){
           return ListView.builder(
             padding: EdgeInsets.only(top:60),
-          itemCount: trashProvider.trashList.length,
-          itemBuilder: (BuildContext context, item){
-              return Container(
+              itemCount: trashProvider.trashList.length,
+              itemBuilder: (BuildContext context, item){
+              return Padding(
               padding: EdgeInsets.fromLTRB(25, 15, 25, 10),
               child: Row(
                 //mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -44,34 +44,38 @@ class BucketList extends StatelessWidget {
         );
         } else {
           return ListView.builder(
+            padding: EdgeInsets.only(top:60),
           itemCount: trashProvider.searchingList.length,
           itemBuilder: (BuildContext context, item){
-              return Container(
-              padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    height: 70,
-                    child: Image(
-                      image: AssetImage('assets/plastic-bottle.png'), 
-                      fit: BoxFit.contain,
-                    )
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(trashProvider.searchingList[item].category, style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
-                        Text(trashProvider.searchingList[item].name,)
-                      ],
+              return Padding(
+                padding: EdgeInsets.fromLTRB(25, 15, 25, 10),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 70,
+                      child: Image(
+                        image: AssetImage('assets/plastic-bottle.png'), 
+                        fit: BoxFit.contain,
+                      )
                     ),
-                  ),
-                ],
-              ),
+                    Container(
+                      padding: EdgeInsets.only(left:25),
+                      width: 200,
+                      child: Text(
+                        trashProvider.searchingList[item].name, 
+                        textAlign: TextAlign.start,
+                        )
+                      ),
+                      Text(
+                      trashProvider.searchingList[item].category, 
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.start,
+                      ),
+                    ],
+                ),
+              );
+          }
             );
-          },
-        );
+          }
         }
   } 
-}
