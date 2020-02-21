5e1095e2f06782f7cbd37b36af66a9eb32320e24
Wroblewski <mwroblewski2@dxc.com>
2020-02-21 14:54:42 +0100

    adjust UI when user search the trash

--- lib/Widget/bucketList.dart
+++ lib/Widget/bucketList.dart
@@ -21,7 +21,8 @@ class BucketList extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20)
                 ),
                 padding: EdgeInsets.fromLTRB(15,25,15,25),
-                child:Row(
+                child: Container(
+                  child: Row(
                     children: <Widget>[
                       Container(
                         padding: EdgeInsets.only(left:20),
@@ -30,67 +31,86 @@ class BucketList extends StatelessWidget {
                           image: AssetImage('assets/plastic-bottle.png'), 
                           fit: BoxFit.contain,
                         )
-                      ),Expanded(
+                      ),
+                      Expanded(
                         child: Container(
-                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Column(
-                          children: <Widget>[
-                            Center(
-                              child: Text(trashProvider.trashList[item].name, 
-                                textAlign: TextAlign.start,
-                                style: TextStyle(
-                                  fontSize: 18
+                          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          child: Column(
+                            children: <Widget>[
+                              Center(
+                                child: Text(trashProvider.trashList[item].name, 
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 18
+                                  ),
                                 ),
                               ),
-                            ),
-                            Text(
-                              trashProvider.trashList[item].category, 
-                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.start,
-                           ),
-                          ],
+                              Text(
+                                trashProvider.trashList[item].category, 
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.start,
+                             ),
+                            ],
+                          ),
                         ),
                       ),
-                      )
                     ],
                   ),
+                ),
             );
           },
         );
         } else {
           return ListView.builder(
             padding: EdgeInsets.only(top:60),
-          itemCount: trashProvider.searchingList.length,
-          itemBuilder: (BuildContext context, item){
-              return Padding(
-                padding: EdgeInsets.fromLTRB(25, 15, 25, 10),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      height: 70,
-                      child: Image(
-                        image: AssetImage('assets/plastic-bottle.png'), 
-                        fit: BoxFit.contain,
-                      )
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(left:25),
-                      width: 200,
-                      child: Text(
-                        trashProvider.searchingList[item].name, 
-                        textAlign: TextAlign.start,
+            itemCount: trashProvider.searchingList.length,
+            itemBuilder: (BuildContext context, item){
+              return Container(
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[100] ,width: 1.9),
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                padding: EdgeInsets.fromLTRB(15,25,15,25),
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(left:20),
+                        height: 70,
+                        child: Image(
+                          image: AssetImage('assets/plastic-bottle.png'), 
+                          fit: BoxFit.contain,
                         )
                       ),
-                      Text(
-                      trashProvider.searchingList[item].category, 
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.start,
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          child: Column(
+                            children: <Widget>[
+                              Center(
+                                child: Text(trashProvider.searchingList[item].name, 
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 18
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                trashProvider.searchingList[item].category, 
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.start,
+                             ),
+                            ],
+                          ),
+                        ),
                       ),
                     ],
+                  ),
                 ),
-              );
-          }
             );
+          },
+        );
           }
         }
   } 
